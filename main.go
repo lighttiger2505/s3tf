a65919486dbd675c50e02a88a1ecee193ba0c192
lighttiger2505 <lighttiger2505@gmail.com>
2018-07-10 07:20:58 +0900

    Add cli framework

--- main.go
+++ main.go
@@ -7,22 +7,41 @@ import (
 	"os"
 
 	"github.com/nsf/termbox-go"
+	"github.com/urfave/cli"
+)
+
+const (
+	ExitCodeOK    int = iota //0
+	ExitCodeError int = iota //1
 )
 
 var bucketList *BucketList
 
-func Init() {
-	bucketList = &BucketList{}
-	bucketList.buckets = ListBuckets()
-	width, height := termbox.Size()
-	fmt.Println(width, height)
-	bucketList.win = newWindow(0, 0, width, height)
-	bucketList.cursorPos = newPosition(0, 0)
-	bucketList.drawPos = newPosition(0, 0)
-
+func main() {
+	err := newApp().Run(os.Args)
+	var exitCode = ExitCodeOK
+	if err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+		exitCode = ExitCodeError
+	}
+	os.Exit(exitCode)
 }
 
-func main() {
+func newApp() *cli.App {
+	app := cli.NewApp()
+	app.Name = "s3 terminal finder"
+	app.HelpName = "s3tf"
+	app.Usage = "AWS S3 TUI file manager"
+	app.UsageText = "s3tf [options] <args>"
+	app.Version = "0.0.1"
+	app.Author = "lighttiger2505"
+	app.Email = "lighttiger2505@gmail.com"
+	app.Flags = []cli.Flag{}
+	app.Action = run
+	return app
+}
+
+func run(c *cli.Context) error {
 	logfile, err := os.OpenFile("./debug.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		panic("cannnot open test.log:" + err.Error())
@@ -33,6 +52,7 @@ func main() {
 	if err := termbox.Init(); err != nil {
 		panic(err)
 	}
+	defer termbox.Close()
 
 	Init()
 	bucketList.Draw()
@@ -54,5 +74,15 @@ mainloop:
 
 		bucketList.Draw()
 	}
-	termbox.Close()
+	return nil
+}
+
+func Init() {
+	bucketList = &BucketList{}
+	bucketList.buckets = ListBuckets()
+	width, height := termbox.Size()
+	fmt.Println(width, height)
+	bucketList.win = newWindow(0, 0, width, height)
+	bucketList.cursorPos = newPosition(0, 0)
+	bucketList.drawPos = newPosition(0, 0)
 }
