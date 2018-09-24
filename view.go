e1fd8d475caf107750465cc0af83501bb4c77f4b
lighttiger2505 <lighttiger2505@gmail.com>
2018-09-25 00:30:45 +0900

    Remove comment out code

--- view.go
+++ view.go
@@ -211,28 +211,3 @@ func (v *ListView) halfPageUp() int {
 func (v *ListView) halfPageDown() int {
 	return v.layer.HalfPageDownCursor(len(v.objects))
 }
-
-type NavigationView struct {
-	Render
-	currentPath string
-	win         *Window
-}
-
-func (v *NavigationView) SetCurrentPath(bucket string, node *model.Node) {
-	if node.IsRoot() {
-		v.currentPath = "list bucket"
-		return
-	}
-
-	showBucketName := fmt.Sprintf("s3://%s", bucket)
-	if node.IsBucketRoot() {
-		v.currentPath = showBucketName
-	} else {
-		v.currentPath = strings.Join([]string{showBucketName, node.Key}, "/")
-	}
-}
-
-func (v *NavigationView) Draw() {
-	str := PadRight(v.currentPath, v.win.Box.Width, " ")
-	tbPrint(0, v.win.DrawY(0), termbox.ColorWhite, termbox.ColorBlue, str)
-}
